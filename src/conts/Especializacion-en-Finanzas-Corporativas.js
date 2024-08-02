8f853010a937ec320e913d1aa057a618433de2d3
germandcvr <gcelis@vereon.co>
2024-08-02 14:37:02 -0500

    Creando componentes reutilizables

---
+++ src/conts/Especializacion-en-Finanzas-Corporativas.js
@@ -0,0 +1,23 @@
+// EFC - Especialización en Finanzas Corporativas
+export const CONFIG = {
+    colorPrincipal: '#182D68',
+    colorSecundario: '#5CC5B3'
+}
+
+export const PROGRAMA = {
+    tipo: 'Especialización en',
+    titulo: 'Finanzas Corporativas',
+    snies: '3840',
+    urlPrograma: 'https://www.cesa.edu.co/escuela-de-negocios/posgrados/especializacion-finanzas-corporativas/',
+    banner: 'efc-banner-2024',
+    whatsApp: 'https://api.whatsapp.com/send?phone=573154191199',
+    inscripcion: 'https://academico.cesa.edu.co/sgacampus/services/inscripciones/home',
+    modalidad: 'Presencial',
+    horario: 
+        `Jueves de 5:30 p. m. a 9:30 p. m. <br/>
+        Viernes de 4:00 p. m. a 8:00 p. m. <br/>
+        Sábado de 8:00 a. m. a 12:00 p. m. 
+        `,
+    intensidad: '2 Semestres',
+    inversion: '$20.928.100'
+}
