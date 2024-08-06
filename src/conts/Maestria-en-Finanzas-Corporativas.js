7986ab73abb4d5cbdc4e73807cae4e9f7f5f56e7
germandcvr <gcelis@vereon.co>
2024-08-06 12:14:41 -0500

    Maestría en Finanzas Corporativas

---
+++ src/conts/Maestria-en-Finanzas-Corporativas.js
@@ -0,0 +1,23 @@
+// MFC - Mestría en finanzas corporativas 
+export const CONFIG = {
+    colorPrincipal: '#08253A',
+    colorSecundario: '#66CCCC'
+}
+
+export const PROGRAMA = {
+    tipo: 'Maestría en',
+    titulo: 'Finanzas Corporativas',
+    snies: '91265',
+    urlPrograma: 'https://www.cesa.edu.co/escuela-de-negocios/posgrados/maestria-finanzas-corporativas/',
+    banner: 'mfc-banner-2024',
+    whatsApp: 'https://api.whatsapp.com/send?phone=573154191199',
+    inscripcion: 'https://academico.cesa.edu.co/sgacampus/services/inscripciones/home',
+    modalidad: 'Presencial',
+    horario: 
+        `Jueves de 5:30 p. m. a 9:30 p. m. <br/>
+        Viernes de 4:00 p. m. a 8:00 p. m. <br/>
+        Sábado de 8:00 a. m. a 12:00 p. m. 
+        `,
+    intensidad: '3 Semestres',
+    inversion: '$24.480.000'
+}
