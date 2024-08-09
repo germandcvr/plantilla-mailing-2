6b5ea400a118264a44e0fac726aebd493ac0d0ad
germandcvr <gcelis@vereon.co>
2024-08-09 08:24:55 -0500

    Administración de Empresas

---
+++ src/conts/Maestria-en-Administracion-de-Empresas.js
@@ -0,0 +1,24 @@
+// MBA - Maestría en Administración de Empresas
+export const CONFIG = {
+    colorPrincipal: '#030b14',
+    colorSecundario: '#fff',
+    colorTercero: '#14387f'
+}
+
+export const PROGRAMA = {
+    tipo: 'Maestría en',
+    titulo: 'Administración de Empresas',
+    snies: '105710',
+    urlPrograma: 'https://www.cesa.edu.co/escuela-de-negocios/posgrados/maestria-administracion-empresas/',
+    banner: 'mba-banner-2024',
+    whatsApp: 'https://api.whatsapp.com/send?phone=573154191199',
+    inscripcion: 'https://academico.cesa.edu.co/sgacampus/services/inscripciones/home',
+    modalidad: 'Presencial',
+    horario: 
+        `Jueves de 5:30 p. m. a 9:30 p. m. <br/>
+        Viernes de 4:00 p. m. a 8:00 p. m. <br/>
+        Sábado de 8:00 a. m. a 12:00 p. m. 
+        `,
+    intensidad: '3 Semestres',
+    inversion: '$28.600.000'
+}
