3253c1d3d1e8446765e415c4417b7b1536fad694
germandcvr <gcelis@vereon.co>
2024-08-29 08:32:58 -0500

    Coffee Break Posgrados

---
+++ src/conts/Coffe-Break-posgrados.js
@@ -0,0 +1,15 @@
+// CBP - Coffe Break Posgrados
+export const CONFIG = {
+    colorPrincipal: '#09193a',
+    colorSecundario: '#0092ff',
+    colorTercero: '#ebbc41',
+}
+
+export const PROGRAMA = {
+    titulo: 'COFFE BREAK de POSGRADOS',
+    snies: '110238',
+    urlPrograma: '#',
+    banner: 'cbp-banner-2024',
+    whatsApp: 'https://api.whatsapp.com/send?phone=573154191199',
+    inscripcion: 'https://academico.cesa.edu.co/sgacampus/services/inscripciones/home',
+}
